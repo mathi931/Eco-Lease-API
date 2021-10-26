f91f1ff66aa365534671082239684f12b0b53180
mathi931 <mesi.mathi@gmail.com>
2021-10-26 12:33:57 +0200

    added folder

--- Lease-API/Controllers/ReservationsController.cs
+++ Lease-API/Controllers/ReservationsController.cs
@@ -1,13 +1,9 @@
-﻿using EcoLease_API.Models;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EcoLease_API.Models;
 using EcoLease_API.Repositories;
 using EcoLease_API.Validators;
-using FluentValidation.Results;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EcoLease_API.Controllers
 {
@@ -16,17 +12,16 @@ namespace EcoLease_API.Controllers
     public class ReservationsController : ControllerBase
     {
         private readonly IReservationRepository _reservationRepository;
+        private readonly IDValidator validatorID = new();
+
+
+        //instance of validator
+        private readonly ReservationValidator validatorReservation = new();
 
         public ReservationsController(IReservationRepository requestRepository)
         {
             _reservationRepository = requestRepository;
         }
-
-
-
-        //instance of validator
-        ReservationValidator validatorReservation = new ReservationValidator();
-        IDValidator validatorID = new IDValidator();
 
         // GET api/Reservations
         [HttpGet]
@@ -39,48 +34,37 @@ namespace EcoLease_API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Reservation>> GetReservation(int id)
         {
-            ValidationResult validation = validatorID.Validate(id);
-            if (!validation.IsValid)
-            {
-                return BadRequest("Wrong Parameters!");
-            }
+            var validation = validatorID.Validate(id);
+            if (!validation.IsValid) return BadRequest("Wrong Parameters!");
 
             return await _reservationRepository.GetByID(id);
         }
 
         // POST api/Reservations
         [HttpPost]
-        public async Task<ActionResult<Reservation>>PostRequest([FromBody] Reservation request)
+        public async Task<ActionResult<Reservation>> PostRequest([FromBody] Reservation request)
         {
-            ValidationResult validation = validatorReservation.Validate(request);
-            ValidationResult validationCustomerID = validatorID.Validate(request.Customer.CID);
-            ValidationResult validationVehicleID = validatorID.Validate(request.Vehicle.VID);
+            var validation = validatorReservation.Validate(request);
+            var validationCustomerID = validatorID.Validate(request.Customer.CID);
+            var validationVehicleID = validatorID.Validate(request.Vehicle.VID);
 
             if (!validation.IsValid || !validationCustomerID.IsValid || !validationVehicleID.IsValid)
-            {
                 return BadRequest("Wrong Parameters!");
-            }
 
             var newRequest = await _reservationRepository.Insert(request);
-            return CreatedAtAction(nameof(GetReservation), new { id = newRequest.RID }, newRequest);
+            return CreatedAtAction(nameof(GetReservation), new {id = newRequest.RID}, newRequest);
         }
 
         // PUT api/Reservations
         [HttpPut]
         public async Task<ActionResult> UpdateReservation(int id, [FromBody] Reservation reservation)
         {
-            ValidationResult validation = validatorReservation.Validate(reservation);
-            ValidationResult validationID = validatorID.Validate(id);
+            var validation = validatorReservation.Validate(reservation);
+            var validationID = validatorID.Validate(id);
 
-            if (id != reservation.RID || !validationID.IsValid)
-            {
-                return BadRequest("Wrong Parameters!");
-            }
+            if (id != reservation.RID || !validationID.IsValid) return BadRequest("Wrong Parameters!");
 
-            if (!validation.IsValid)
-            {
-                return BadRequest("Wrong Parameters!");
-            }
+            if (!validation.IsValid) return BadRequest("Wrong Parameters!");
 
             await _reservationRepository.Update(reservation);
 
@@ -91,17 +75,11 @@ namespace EcoLease_API.Controllers
         [HttpPut("status")]
         public async Task<ActionResult> UpdateReservationStatus(int id, [FromBody] string status)
         {
-            ValidationResult validationID = validatorID.Validate(id);
-            if (!validationID.IsValid)
-            {
-                return BadRequest("Wrong Parameters!");
-            }
+            var validationID = validatorID.Validate(id);
+            if (!validationID.IsValid) return BadRequest("Wrong Parameters!");
 
             var reservationToUpdate = await _reservationRepository.GetByID(id);
-            if (reservationToUpdate == null)
-            {
-                return BadRequest();
-            }
+            if (reservationToUpdate == null) return BadRequest();
             await _reservationRepository.UpdateStatus(id, status);
 
             return NoContent();
@@ -111,22 +89,17 @@ namespace EcoLease_API.Controllers
         [HttpDelete("{id}")]
         public async Task<ActionResult> Delete(int id)
         {
-            ValidationResult validationID = validatorID.Validate(id);
-            if (!validationID.IsValid)
-            {
-                return BadRequest("Wrong Parameters!");
-            }
+            var validationID = validatorID.Validate(id);
+            if (!validationID.IsValid) return BadRequest("Wrong Parameters!");
 
             var reservationToDelete = await _reservationRepository.GetByID(id);
             if (reservationToDelete == null)
             {
                 return NotFound();
             }
-            else
-            {
-                await _reservationRepository.Remove(reservationToDelete.RID);
-                return NoContent();
-            }
+
+            await _reservationRepository.Remove(reservationToDelete.RID);
+            return NoContent();
         }
     }
 }
